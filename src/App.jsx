5623de5794f2db01294a2cc0be32a68147b0baa2
Amit212119 <amgiri1010@gmail.com>
2023-11-06 21:25:28 +0530

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dashboard from './components/dashboard'
+
+const App = () => {
+  return (
+    <div><Dashboard /></div>
+  )
+}
+
+export default App
---
+++ src/components/dashboard.jsx
@@ -0,0 +1,182 @@
+import { useState } from 'react';
+import TableRow from './tableRow';
+
+const Dashboard = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const recPerPage = 10;
+  const lastIndex = currentPage * recPerPage;
+  const firstIndex = lastIndex - recPerPage;
+
+  const numbers = 15;
+
+  function changePage(id) {
+    setCurrentPage(id);
+  }
+  function prePage() {
+    if (currentPage !== firstIndex) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  function nextPage() {
+    if (currentPage !== lastIndex) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  return (
+    <section className='bg-gray-50 dark:bg-gray-900 p-3 sm:p-5'>
+      <div className='mx-auto max-w-screen-xl px-4 lg:px-12'>
+        <div className='bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden'>
+          <div className='overflow-x-auto'>
+            <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+              <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                <tr>
+                  <th
+                    scope='col'
+                    className='px-4 py-3'>
+                    Product name
+                  </th>
+                  <th
+                    scope='col'
+                    className='px-4 py-3'>
+                    Category
+                  </th>
+                  <th
+                    scope='col'
+                    className='px-4 py-3'>
+                    Brand
+                  </th>
+                  <th
+                    scope='col'
+                    className='px-4 py-3'>
+                    Description
+                  </th>
+                  <th
+                    scope='col'
+                    className='px-4 py-3'>
+                    Price
+                  </th>
+                  <th
+                    scope='col'
+                    className='px-4 py-3'>
+                    <span className='sr-only'>Actions</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((row, index) => {
+                  return (
+                    <TableRow
+                      index={index}
+                      key={row.email}
+                      row={row}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <nav
+            className='flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4'
+            aria-label='Table navigation'>
+            <span className='text-sm font-normal text-gray-500 dark:text-gray-400'>
+              Showing
+              <span className='font-semibold text-gray-900 dark:text-white'>1-10</span>
+              of
+              <span className='font-semibold text-gray-900 dark:text-white'>1000</span>
+            </span>
+            <ul className='inline-flex items-stretch -space-x-px'>
+              <li>
+                <a
+                  href='#'
+                  onClick={prePage}
+                  className='flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                  <span className='sr-only'>Previous</span>
+                  <svg
+                    className='w-5 h-5'
+                    aria-hidden='true'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      fillRule='evenodd'
+                      d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                </a>
+              </li>
+              {numbers.map((n, i) => (
+                <li key={i}>
+                  <a
+                    href='#'
+                    onClick={changePage}
+                    className='flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                    {n}
+                  </a>
+                </li>
+              ))}
+
+              {/* <li>
+                <a
+                  href='#'
+                  className='flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                  1
+                </a>
+              </li> */}
+              {/* <li>
+                <a
+                  href='#'
+                  className='flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                  2
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  aria-current='page'
+                  className='flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'>
+                  3
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                  ...
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                  100
+                </a>
+              </li> */}
+              <li>
+                <a
+                  href='#'
+                  onClick={nextPage}
+                  className='flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                  <span className='sr-only'>Next</span>
+                  <svg
+                    className='w-5 h-5'
+                    aria-hidden='true'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      fillRule='evenodd'
+                      d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Dashboard;
